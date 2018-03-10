e2bbe03412109fb634fc72bb0b8437ac5ab915e5
chockeyBlandness <1003340506@qq.com>
2018-03-10 13:49:52 +0800

    Initial commit

---
+++ src/main/resources/static/js/view.js
@@ -0,0 +1,10 @@
+angular.module("indexView",['ui.router'])
+.config(function ($stateProvider) {
+    var registerView={
+        name:'register',
+        url:'/register',
+        templateUrl:'registerPage.html'
+    }
+
+    $stateProvider.state(registerView);
+});
